810d74c2df1aee129bd7e55e96254bb15814819a
dishu001 <2020eeb1167@iitrpr.ac.in>
2023-12-05 22:36:31 +0530

    test1

---
+++ scripts/loader.js
@@ -0,0 +1,33 @@
+// It is loader module: Just Add this script at the end of the page
+
+
+const loaderInit = () => {
+    let loaderContainer = document.createElement('div');
+    loaderContainer.className = "bg-white h-100vh d-flex justify-content-center align-items-center w-100 flex-column bg-logo position-fixed top-0 loader_container";
+    loaderContainer.id = "main_loader";
+    // Create an img element
+    let img = document.createElement('img');
+    img.src = "/assets/news/footer_logo.svg";
+    img.alt = "Main_logo";
+    img.srcset = "/assets/news/footer_logo.svg";
+    img.className = "loader_anm";
+
+    // Create an h1 element
+    let h1 = document.createElement('h1');
+    h1.className = "fw-bold mt-5 h1 loader_anm anim-typewriter line-1";
+    h1.textContent = "Department of Electrical Engineering";
+
+    // Append img and h1 to loaderContainer
+    loaderContainer.appendChild(img);
+    loaderContainer.appendChild(h1);
+
+    // Append loaderContainer to the body
+    document.body.appendChild(loaderContainer);
+}
+loaderInit();
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        let elem = document.getElementById("main_loader");
+        elem.remove();
+    }, 1000)
+});
---
+++ scripts/research.highlights.js
@@ -0,0 +1,36 @@
+// const ResearchHighlightData = [{
+//         title: "Publications (>90% are IEEE)",
+//         value: 510,
+//         illustration: "./../assets/publication.png",
+//     },
+//     {
+//         title: "Patents",
+//         value: 11,
+//         illustration: "./../assets/patents.png",
+//     },
+//     {
+//         title: "Cross Funding(INR)",
+//         value: 48,
+//         illustration: "./../assets/cros_funding.png",
+//     },
+//     {
+//         title: "Projects",
+//         value: 61,
+//         illustration: "./../assets/projects.png",
+//     },
+// ];
+
+
+// window.onload = function() {
+//     const research_highlights = () => {
+//         let elem = document.getElementById("research_highlight_content");
+//         ResearchHighlightData.forEach((item, i) => {
+//             elem.innerHTML += `
+//                 <div class="research_card">
+//                     <img src="${item.illustration}" alt="" width="50%" />
+//                     <span>${item.value}</span>
+//                 </div>`;
+//         });
+//     };
+//     research_highlights();
+// };
